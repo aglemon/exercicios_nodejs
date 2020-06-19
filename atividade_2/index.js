61905d54c74942b617de090fdc8227db9b315e59
aglemon <aglemonwork@gmail.com>
2020-06-19 20:49:37 -0300

    atividade 2 completa

--- atividade_2/index.js
+++ atividade_2/index.js
@@ -1,5 +1,17 @@
-// ## Importe a biblioteca ##
+// Importe a biblioteca ##
+let rs = require('readline-sync');
 
+// Perguntar se o usuário está usando máscara quando precisa sair de casa (Sim ou Não)
+// Perguntar se o usuário está lavando as mãos frequentemente
+// Se o usuário responder sim para as perguntas anteriores, mostrar um elogio para o usuário
+// Se não, dar bronca no usuário
 
+let respostaUsandoMascara = rs.question('Está usando máscara (Sim ou Nao)?\n ');
+let respostaLavandoMaos   = rs.question('está lavando as maos frequentemente (Sim ou Nao)?\n')
 
-// ## Faça o código ##
+if ((respostaUsandoMascara == "sim" ) && (respostaLavandoMaos == "sim")) {
+    console.log('Parabens\n');
+}
+else{
+    console.log('SHAME ON YOU\n');
+}
