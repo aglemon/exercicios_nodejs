1d2d9912b43dd343c373c7b6aaa078cad302b825
aglemon <aglemonwork@gmail.com>
2020-06-19 21:12:46 -0300

    atividade 3 feita

--- atividade_3/index.js
+++ atividade_3/index.js
@@ -1,5 +1,17 @@
 // ## Importe a biblioteca ##
+let rs = require('readline-sync');
+
+// Criar uma aplicação que calcula a área de um quadrado
+// A aplicação deve:
+// Perguntar para o usuário o tamanho dos lados dos quadrado
+// Calcular sua área
+// Informar a área calculada ao usuário## Faça o código ##
+let ladoDoQuadrado = rs.question('Qual o tamanho dos lados do quadrado?\n');
+
+// area = L elevado a 2
+let areaDoQuadrado = Math.pow(ladoDoQuadrado, 2);
+
+console.log('A area total do quadrado e ' + areaDoQuadrado);
 
 
 
-// ## Faça o código ##
